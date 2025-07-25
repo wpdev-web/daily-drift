4cb583b47e73c7ea9b30475fad32cfa4f54421b2
wpdev-web <haidersoft2020@gmail.com>
2025-07-25 23:53:01 +0530

    Add MongoDB connection and error handling; implement post model and update feed routes

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const path= require('path');
 
 const feedRoutes = require('./routes/feed');
 
@@ -7,6 +9,7 @@ const app = express();
 
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
+app.use('/images', express.static(path.join(__dirname,'images'))); // Serve static files from 'images' directory
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -16,5 +19,19 @@ app.use((req, res, next) => {
 });
 
 app.use('/feed', feedRoutes);
+
+app.use((error, req, res, next) => {
+    console.error('Error:', error);
+    const status = error.statusCode || 500;
+    const message = error.message || 'An error occurred.';
+    res.status(status).json({ message: message });
+});
+
+mongoose.connect('mongodb+srv://haidersoft2020:XTd1F4nY36pZq0sm@cluster0.qkxzxym.mongodb.net/bloging?retryWrites=true&w=majority&appName=Cluster0')    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch(err => {
+        console.error('MongoDB connection error:', err);
+    });
 
 app.listen(8080);
--- backend/controllers/feed.js
+++ backend/controllers/feed.js
@@ -1,4 +1,5 @@
 const { validationResult } = require('express-validator');
+const Post = require('../modal/post');
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [{ 
@@ -15,22 +16,32 @@ exports.getPosts = (req, res, next) => {
 exports.createPost = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res.status(422).json({
-      message: 'Validation failed.',
-      errors: errors.array()
-    });
+    const error=new Error('Validation failed, entered data is incorrect.');
+    error.statusCode = 422; // Unprocessable Entity
+    throw error;
   }
   const title = req.body.title;
   const content = req.body.content;
-  // Create post in db
-  res.status(201).json({
-    message: 'Post created successfully!',
-    post: { 
-       _id: new Date().toISOString(), 
-       title: title, 
-       content: content,
-       creator: { name: 'John Doe' },
-       createdAt: new Date().toISOString() 
-      }
+  const post = new Post({
+    title: title,
+    content: content,
+    imageUrl: 'images/Foto.jpg',
+    creator: { name: 'John Doe' }
   });
+  post.save()
+    .then(result => {
+      console.log('Post created:', result);
+        res.status(201).json({
+          message: 'Post created successfully!',
+          post: result
+      });
+    })
+    .catch(err => {
+      console.error('Error creating post:', err);
+      if (!err.statusCode) {
+        err.statusCode = 500; // Internal Server Error
+      }
+      next(err);
+    });
+
 };
